29aab4b0b904b52414787eff675b77d84c9ef464
Takashi Menjo <takashi.menjo+github@gmail.com>
2016-02-09 16:24:44 +0900

    Add make.

--- apt-get-prereqs-sheepdog.sh
+++ apt-get-prereqs-sheepdog.sh
@@ -2,4 +2,4 @@
 set -x
 export LANG=C LC_ALL=C
 sudo apt-get --quiet -y install \
-  git pkg-config autoconf libtool yasm liburcu-dev libzookeeper-mt-dev
+  git pkg-config make autoconf libtool yasm liburcu-dev libzookeeper-mt-dev

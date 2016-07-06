cee15b35f088ebe63de18b3a68b599b8c3ed1d44
Takashi Menjo <takashi.menjo+github@gmail.com>
2016-07-06 16:04:37 +0900

    Clean-up

--- build-sheepdog.sh
+++ build-sheepdog.sh
@@ -4,13 +4,12 @@ export LANG=C LC_ALL=C
 (
   cd ~
   if [ ! -d sheepdog ] ; then
-    git clone "${SHEEPDOG_GITREPO:-https://github.com/sheepdog/sheepdog.git}"
+    git clone https://github.com/tmenjo/sheepdog.git
   fi
   cd sheepdog
-  git checkout "${SHEEPDOG_GITBRANCH:-master}"
   ./autogen.sh
   ./configure --prefix=/usr --localstatedir=/var \
     --enable-zookeeper --disable-corosync
   make
-  sudo make install-exec
+  sudo make install
 )
